9607bb20761fa4052248302f62acdc527e5fcbaa
samuel1010111 <samuel.t.abebe@gmail.com>
2024-03-27 01:28:05 +0200

    header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,42 @@
 
+import { Link } from 'react-router-dom';
+
+// Import image files
+import facebook from '../asserts/media/facebook.png';
+import twitter from '../asserts/media/twitter.png';
+import instagram from '../asserts/media/instagram.png';
+import whatsapp from '../asserts/media/whatsapp.png';
+import youtube from '../asserts/media/youtube.png';
+
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+  // Define MediaData array with imported image files
+  const mediaData = [
+    { href: "https://www.facebook.com", img: facebook },
+    { href: "https://twitter.com", img: twitter },
+    { href: "https://www.instagram.com", img: instagram },
+    { href: "https://whatsapp.com", img: whatsapp },
+    { href: "https://www.youtube.com/@gooddomicroprojects/", img: youtube },
+  ];
+
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <h className="footer-title">© 2023 All rights reserved.</h>
+        <nav className="footer-nav">
+          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/terms">Terms of Service</Link>
+        </nav>
+        <h className="footer-title">Cookies Setting</h>
+      </div>
+      <div className="media-icons">
+        {mediaData.map((item, index) => (
+          <a href={item.href} target="_blank" key={index}>
+            <img src={item.img} alt={item.title} className="media-icon" />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/components/MediaData.js
@@ -0,0 +1,35 @@
+import facebook from '../asserts/media/facebook.png'
+import twitter from '../asserts/media/twitter.png'
+import instagram from '../asserts/media/instagram.png'
+import whatsapp from '../asserts/media/whatsapp.png'
+import youtube from '../asserts/media/youtube.png'
+
+
+export const MediaData = [
+    {
+        img:youtube,
+        title: "youtube",
+        href:"https://www.youtube.com/@gooddomicroprojects/"
+      },
+      {
+        img:facebook,
+        title: "facebook",
+        href: "https://www.facebook.com"
+      },
+      {
+        img:instagram,
+        title: "instagram",
+        href: "https://www.instagram.com"
+      },
+      {
+        img:twitter,
+        title: "twitter",
+        href: "https://twitter.com"
+      },
+    
+      {
+        img:whatsapp,
+        title: "whatsapp",
+        href: "https://whatsapp.com"
+      },
+]
