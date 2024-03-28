c71bc428b90a51fdbe759a12ab8fc0c78c91b396
Alona Chubenko <aliona.chubenko@gmail.com>
2024-03-28 12:26:27 +0200

    Change style of login and register pages

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -10,30 +10,30 @@ const Login = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
         <div className="flex flex-col justify-center p-8 md:p-14">
-          <span className="mb-3 text-4xl font-bold text-center">Log in</span>
+          <span className="mb-3 text-4xl font-bold text-center text-fontDark">Log in</span>
           <div className="py-3">
             <input
-              className="w-full p-2 md:w-64 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+              className="w-full p-2 md:w-64 bg-whiteReplacement border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
               type="email"
               placeholder="Enter your email"
             />
           </div>
           <div className="py-3">
             <input
-              className="w-full p-2 md:w-64 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+              className="w-full p-2 md:w-64 bg-whiteReplacement border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
               type="password"
               placeholder="Enter password"
             />
           </div>
           <Button
             onClick={login}
-            className="w-full border border-transparent bg-gray-900 text-white p-2 rounded-lg mb-6 hover:bg-gray-500"
+            className="w-full border border-transparent bg-mainBackground text-white p-2 rounded-lg mb-6 hover:bg-smallBackground"
           >
             Login
           </Button>
           <div className="text-center text-gray-400">
             Don't have an account?
-            <span className="cursor-pointer font-bold text-black">
+            <span className="cursor-pointer font-bold text-fontDark">
               {" "}
               Sign up
             </span>
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -11,30 +11,30 @@ const Register = () => {
 <div className="flex items-center justify-center min-h-screen bg-gray-100">
 <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 ">
 <div className="flex flex-col justify-center p-8 md:p-14">
-<span className="mb-3 text-4xl font-bold text-center">Sign up</span>
+<span className="mb-3 text-4xl font-bold text-center text-fontDark">Sign up</span>
 <div className="py-3">
 <input
-className="w-full md:w-64 p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+className="w-full md:w-64 p-2 bg-whiteReplacement border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
 type="text"
 placeholder="Enter full name"
  />
 </div>
 <div className="py-3">
 <input
-className=" w-full p-2 md:w-64 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+className=" w-full p-2 md:w-64 bg-whiteReplacement border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
 type="text"
 placeholder="Enter email"
  />
 </div>
 <div className="py-3">
 <input
-className="w-full p-2 md:w-64 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+className="w-full p-2 md:w-64 bg-whiteReplacement border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
 type="password"
 id="password"
 placeholder="Enter password"/>
 </div>
 
-<Button className="w-full border border-transparent bg-gray-900 text-white p-2 rounded-lg mb-6 hover:bg-gray-500"
+<Button className="w-full border border-transparent bg-mainBackground text-white p-2 rounded-lg mb-6 hover:bg-smallBackground"
  onClick={register}>Sign up</Button>
 </div>
 
