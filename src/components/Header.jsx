b73553d9e78a07c4a07e89818c17fe03dc841b17
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-04-02 09:51:14 +0300

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
-import { Navbar } from "react-bootstrap"
+import "../components/footerHeader.css";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-        </div>
-    )
-}
 
-export default Header
+    return (
+
+
+        <div className="Header-container">
+            <div className='Header-content'>
+                <nav className="navbar">
+
+                    <Link to="/">Home</Link>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/calendar">Calendar</Link>
+                    <Link to="/edit">Edit</Link>
+
+                </nav>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default Header;
