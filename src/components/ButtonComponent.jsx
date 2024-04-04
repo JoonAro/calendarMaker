efde7efc6da5084fd473d0b7321739a2f027a325
Alona Chubenko <aliona.chubenko@gmail.com>
2024-04-04 11:37:53 +0300

    Add button component

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,14 @@
+const ButtonComponent = () => {
+    return (
+    
+        <button
+              // onClick={submitHandler}
+              className="w-36 border border-transparent bg-mainBackground text-white p-2 rounded-lg mb-6 hover:bg-smallBackground"
+            >
+              Submit
+            </button>
+     
+    )
+  }
+  
+  export default ButtonComponent;
