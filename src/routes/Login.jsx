4c4052e3bf35e8fbf55878e80a538f0d5152c8b5
Alona Chubenko <aliona.chubenko@gmail.com>
2024-04-04 12:11:07 +0300

    Change background in login and register pages

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-mainBackground">
       <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
         <div className="flex flex-col justify-center p-8 md:p-14">
           <span className="mb-3 text-4xl font-bold text-center text-fontDark">Log in</span>
