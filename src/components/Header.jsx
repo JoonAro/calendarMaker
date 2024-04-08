52db1e4f956aa38c08d4a00e810e91cb57a89fa5
samuel1010111 <samuel.t.abebe@gmail.com>
2024-04-08 10:29:12 +0300

    changed the colour

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,18 @@
-import "../styles/footerHeader.css";
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-
     return (
-
-
-        <div className="Header-container fixed top-0 w-full z-10">
-            <div className='Header-content'>
-                <nav className="navbar">
-
-                    <Link to="/">Home</Link>
-                    <Link to="/register">Register</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/calendar">Calendar</Link>
-                    <Link to="/editorPage">Edit</Link>
-
+        <div className="Header-container fixed top-0 w-full z-10 bg-smallBackground text-black flex justify-end items-center h-16">
+            <div className='Header-content flex justify-end w-full sm:ml-0 sm:mr-0'>
+                <nav className="navbar flex justify-end w-full">
+                    <Link to="/" className="mx-4">Home</Link>
+                    <Link to="/register" className="mx-4">Register</Link>
+                    <Link to="/login" className="mx-4">Login</Link>
+                    <Link to="/calendar" className="mx-4">Calendar</Link>
+                    <Link to="/editorPage" className="mx-4">Edit</Link>
                 </nav>
             </div>
         </div>
-
-
     );
 };
 
