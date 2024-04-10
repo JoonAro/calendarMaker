6d6a86d627156f83116e87498b5e0319ee7c840a
joonaro <cupetus@gmail.com>
2024-04-10 14:39:26 +0300

    added hatch nr

--- src/components/DoubleHatch.jsx
+++ src/components/DoubleHatch.jsx
@@ -16,6 +16,9 @@ const DoubleHatch = ({ hatch }) => {
             backgroundImage: showImage ? `url("${hatch.hatchImg}")` : 'url("")'
         }} className={`calendarImage `}>
             <div onClick={openHatch} className={`hatch left ${clicked ? 'openStyle' : 'closedStyle'}`}>
+                <p style={{
+                    color: 'whitesmoke'
+                }}>{hatch.hatchNr}</p>
             </div>
             <div onClick={openHatch} className={`hatch right ${clicked ? 'openStyle' : 'closedStyle'}`}>
             </div>
