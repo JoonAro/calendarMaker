f14a75e19f5621adcdcb297204ce44f97536bc78
joonaro <cupetus@gmail.com>
2024-04-14 12:59:14 +0300

    created calendarComponent & moved gridholder div from editor to components

--- src/components/ImageCatalogue.jsx
+++ src/components/ImageCatalogue.jsx
@@ -1,8 +1,10 @@
 import TextComponent from "./TextComponent"
 
-const ImageCatalogue = ({ bool, images, handleBgSelection, guideH, guideText }) => {
+const ImageCatalogue = ({ bool, images, handleBgSelection, guideH, guideText, calendarImage }) => {
     return (
-        <>
+        <div className="gridHolder" style={{
+            backgroundImage: `url(${calendarImage})`,
+        }}>
             {bool && <TextComponent guideH={guideH} guideText={guideText} />}
             {!bool &&
                 <TextComponent guideH={guideH} guideText={guideText} />
@@ -12,7 +14,7 @@ const ImageCatalogue = ({ bool, images, handleBgSelection, guideH, guideText }) 
                     backgroundImage: `url(${hatchImg.urls.small})`
                 }}></div>
             })}
-        </>
+        </div>
     )
 }
 
