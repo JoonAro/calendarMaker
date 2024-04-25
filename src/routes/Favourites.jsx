4abab83038814db6017e784b8720d2d30bb21133
Alona Chubenko <aliona.chubenko@gmail.com>
2024-04-25 13:36:25 +0300

    style for favourites cards

--- src/routes/Favourites.jsx
+++ src/routes/Favourites.jsx
@@ -3,6 +3,7 @@ import { getDocs, collection } from "firebase/firestore";
 import { auth, db } from "../auth/firebase";
 import { Card, Container, Row } from 'react-bootstrap';
 import ButtonComponent from '../components/ButtonComponent';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Favorites = () => {
     const [usersCalendar, setUsersCalendar] = useState([]);
@@ -24,22 +25,30 @@ const Favorites = () => {
 
     return (
         <>
-            <Container fluid style={{ background: "#67595E", minHeight: "100vh" }}>
-                <Row xs={1} md={1} lg={1} className="g-3">
-                    <div className="d-flex flex-wrap justify-content-around mt-3">
+            <Container className='flex min-h-screen items-center justify-center m-5'>
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+
+
                         {usersCalendar.map(calendar => (
-                            <Card key={calendar.id} className="m-2" style={{ backgroundColor: "#EED6D3", width: '18rem' }}>
-                                <Card.Img variant="top" src={calendar.data.bgImage} />
-                                <div className="d-flex justify-content-center">
-                                    <ButtonComponent>Preview</ButtonComponent>
+                            <Card key={calendar.id} className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+
+                                <div className='h-96 w-72'>
+                                <Card.Img className='h-full w-full object-cover group-hover:rotate-3 group-hover:scale-125 transition-transform duration-500' src={calendar.data.bgImage} />
+
+                                <CloseIcon className="absolute text-muted top-0 right-0 bg-smallBackground opacity-75 p-1"/>
                                 </div>
-                                <div className="d-flex justify-content-center">
-                                    <ButtonComponent>Edit</ButtonComponent>
+                                
+                                <div className='absolute insent-0 flex flex-col items-center justify-center px-9 text-center translate-y-[60%] group-hover:translate-y-0 tarnsition-all duration-500 bottom-0'>
+
+                                <ButtonComponent className="p-1">Preview</ButtonComponent>
+                                <ButtonComponent>Edit</ButtonComponent>
                                 </div>
+                                
                             </Card>
+                           
                         ))}
-                    </div>
-                </Row>
+                         </div>
+             
             </Container>
         </>
     );
