bd5783ebc55563f4ba7647bb4d790ea69aa1cd6f
samuel1010111 <samuel.t.abebe@gmail.com>
2024-05-13 12:06:58 +0300

    correcting the codes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,21 @@
 import cale from '../assets/media/cale.jpg';
 import ca from '../assets/media/sa.jpg';
 import { Link } from 'react-router-dom';
-import mastercard from '../assets/media/money/mastercard.png';
+{/*import mastercard from '../assets/media/money/mastercard.png';
 import paypal from '../assets/media/money/paypal.png';
 import visa from '../assets/media/money/visa.png';  
+*/}
 
 const Footer = () => {
   return (
     <div className=' row bg-accentColor-light text-dark'>
-      <div className='container mx-auto py-12 '>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-16'>
+      <div className='container mx-auto py-12  '>
+        <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4 md:gap-16'>
           <div className='py-8 '> {/* Add padding here */}
             <div className='header flex  '>
               <h1 className='text-xl font-bold mb-4'>Dream Calendar</h1>
             </div>
-            <p className='text-base mb-4 ml-'>Dream calendar works to make your dreams come true by planning your day. You shape your life, you style your calendar the way you want. We are here to help you reach your goals.</p>
+            <p className='text-base mb-4 w-40'>Dream calendar works to make your dreams come true by planning your day. You shape your life, you style your calendar the way you want. We are here to help you reach your goals.</p>
           </div>
           <div className=' py-8  '> {/* Add padding here */}
             <h3 className='text-lg font-semibold mb-4'>Our Information</h3>
@@ -31,21 +32,17 @@ const Footer = () => {
         </div>
         <div className='row'>
           <div className='col-md-6 flex items-center mt-12 '>
-      
-          <h3>© Copyright 2024 Dream Calendar, Inc. All rights reserved</h3>
-            
+            <h3>© Copyright 2024 Dream Calendar, Inc. All rights reserved</h3>
           </div>
-      
-          <div className='col-md-6 flex items-center justify-end mt-12'>
-  <img src={mastercard} alt='payimg' className='w-16 h-10 mr-4' />
-  <img src={paypal} alt='payimg' className='w-16 h-10 mr-4' />
-  <img src={visa} alt='payimg' className='w-16 h-10' />
-</div>
-
+          {/*<div className='col-md-6 flex items-center justify-end mt-12'>
+            <img src={mastercard} alt='payimg' className='w-16 h-10 mr-4' />
+            <img src={paypal} alt='payimg' className='w-16 h-10 mr-4' />
+            <img src={visa} alt='payimg' className='w-16 h-10' />
           </div>
+          */}
         </div>
       </div>
-    
+    </div>
   );
 }
 
