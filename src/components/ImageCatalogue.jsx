9f271bdb95e5ba68d864ad41d7ab4a0873b5f7be
joonaro <cupetus@gmail.com>
2024-05-13 16:29:18 +0300

    hatchImageCatalogue added

--- src/components/ImageCatalogue.jsx
+++ src/components/ImageCatalogue.jsx
@@ -2,24 +2,24 @@ import PickTheDate from "./PickTheDate"
 import TextComponent from "./TextComponent"
 import Search from "./Search"
 
-const ImageCatalogue = ({ bool, images, searchInput, handleSearch, handleBgSelection, handleStartDate, handleEndDate, guideH, guideText, calendarImage, startDate, endDate, time, handleDatePick, hatchEdit, handleHatchImgSelect }) => {
+const ImageCatalogue = ({ bool, images, searchInput, handleSearch, handleBgSelection, handleStartDate, handleEndDate, guideH, guideText, calendarImage, startDate, endDate, time, handleDatePick }) => {
     return (
         <div className="calendarHolder">
-            {hatchEdit && <Search searchInput={searchInput} handleSearch={handleSearch} />}
+            {/* {hatchEdit && <Search searchInput={searchInput} handleSearch={handleSearch} />} */}
             <div className="catalogueHolder" >
                 {!bool && <PickTheDate searchInput={searchInput} handleSearch={handleSearch} handleStartDate={handleStartDate} handleEndDate={handleEndDate} startDate={startDate} endDate={endDate} guideH={guideH} guideText={guideText} yes={""} no={""} time={time} handleDatePick={handleDatePick} />}
                 {bool && <TextComponent guideH={guideH} guideText={guideText} yes={""} no={""} />}
-                {bool && !hatchEdit && images.map(hatchImg => {
+                {bool && images.map(hatchImg => {
                     return <div className="catalogueImage" key={hatchImg.id} onClick={() => handleBgSelection(hatchImg)} style={{
                         backgroundImage: `url(${hatchImg.urls.regular})`
                     }}></div>
                 })}
-                {hatchEdit &&
+                {/* {hatchEdit &&
                     images.map(hatchImg => {
                         return <div className="catalogueImage" key={hatchImg.id} onClick={() => handleHatchImgSelect(hatchImg)} style={{
                             backgroundImage: `url(${hatchImg.urls.regular})`
                         }}></div>
-                    })}
+                    })} */}
             </div>
         </div>
     )
