1a359bf5c4db4e792e8c94307cfef60ad88fc99b
samuel1010111 <samuel.t.abebe@gmail.com>
2024-05-13 09:27:14 +0300

    styled the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,16 @@ import visa from '../assets/media/money/visa.png';
 
 const Footer = () => {
   return (
-    <div className=' row bg-gray-800 text-white'>
+    <div className=' row bg-accentColor-light text-dark'>
       <div className='container mx-auto py-12 '>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-16'>
-          <div className='py-8'> {/* Add padding here */}
-            <div className='header flex '>
+          <div className='py-8 '> {/* Add padding here */}
+            <div className='header flex  '>
               <h1 className='text-xl font-bold mb-4'>Dream Calendar</h1>
             </div>
-            <p className='text-base mb-4 ml-0'>Dream calendar works to make your dreams come true by planning your day. You shape your life, you style your calendar the way you want. We are here to help you reach your goals.</p>
+            <p className='text-base mb-4 ml-'>Dream calendar works to make your dreams come true by planning your day. You shape your life, you style your calendar the way you want. We are here to help you reach your goals.</p>
           </div>
-          <div className=' py-8 '> {/* Add padding here */}
+          <div className=' py-8  '> {/* Add padding here */}
             <h3 className='text-lg font-semibold mb-4'>Our Information</h3>
             <Link to='/contact' className='block mb-2'>Contact Us</Link>
             <Link to='/privacy' className='block mb-2'>Privacy policy</Link>
