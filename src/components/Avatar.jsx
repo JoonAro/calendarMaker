a3987101af0e546de3aa64cecd84d7ec4ed3e333
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-05-16 17:53:47 +0300

    resizing images, alt to images and avatar function

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,24 +1,28 @@
+
 import { useMemo } from 'react';
 import { createAvatar } from '@dicebear/core';
 import { funEmoji } from '@dicebear/collection';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 function Avatar({ avatarValue }) {
-    const avatar = useMemo(() => {
+    const avatarDataUri = useMemo(() => {
         return createAvatar(funEmoji, {
             size: 40,
-            seed: avatarValue,
-
+            seed: avatarValue
         }).toDataUriSync();
     }, [avatarValue]);
 
     return (
         <img
-            src={avatar}
-            alt={`Avatar ${avatarValue}`}
+            src={avatarDataUri}
+            alt={`Avatar ${avatarValue} `}
             style={{ borderRadius: '50%', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
         />
     );
 }
+
+Avatar.propTypes = {
+    avatarValue: PropTypes.string.isRequired
+};
 
 export default Avatar;
