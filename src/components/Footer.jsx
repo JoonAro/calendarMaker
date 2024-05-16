8c751672610054283c8ace8a42475816249ca5be
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-05-16 17:53:47 +0300

    resizing images, alt to images and avatar function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,14 +11,14 @@ const Footer = () => {
   return (
     <div className={`footer ${theme === 'dark' ? 'bg-smallBackground-dark' : 'bg-accentColor-light'}`}>
       <div className="container mx-auto py-12 px-4 md:px-0">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 "> {/* Adjusted columns and added gap */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
           <div>
             <div className="header flex items-center">
               <h1 className={`text-xl font-bold mb-4 ${fontDark}`}>
                 Dream Calendar
               </h1>
             </div>
-            
+
             <Link to="/privacy" className={`block mb-2 ${whiteReplacement} ${fontDark}`}>Privacy policy</Link>
             <Link to="/licence" className={`block mb-2 ${whiteReplacement} ${fontDark}`}>License</Link>
             <Link to="/terms" className={`block mb-2 ${whiteReplacement} ${fontDark}`}>Terms & conditions</Link>
@@ -30,7 +30,7 @@ const Footer = () => {
             <Link to="/contact" className={`block mb-2 ${whiteReplacement} ${fontDark}`}>Feedback</Link>
             <Link to="/faq" className={`block mb-2 ${whiteReplacement} ${fontDark}`}>FAQ</Link>
           </div>
-          
+
           <div className="footer-item">
             <h3 className={`text-lg font-semibold mb-4 ${fontDark}`}>
               Our Information
@@ -39,12 +39,12 @@ const Footer = () => {
             <Link to="/premium" className={`block mb-2 ${whiteReplacement} ${fontDark}`}>premium</Link>
           </div>
           <div className="footer-item">
-           
-           
-            <Link to="/premium" className={`text-lg font-semibold mb-4f ${whiteReplacement} ${fontDark}`}> </Link>
-            </div>
 
-          
+
+            <Link to="/premium" className={`text-lg font-semibold mb-4f ${whiteReplacement} ${fontDark}`}> </Link>
+          </div>
+
+
         </div>
         <div className="flex justify-between items-center mt-16">
           <h3 className={fontDark}>
